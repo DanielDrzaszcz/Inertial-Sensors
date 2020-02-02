1c674d00977fc920d511c80de68342c928b32da3
DanielDrzaszcz <dandrzas@gmail.com>
2020-02-02 14:32:06 +0100

    graphView dodanie osi akcelerometru x/Z

--- app/src/main/java/com/dandrzas/inertialsensorsviewer/MainActivity.java
+++ app/src/main/java/com/dandrzas/inertialsensorsviewer/MainActivity.java
@@ -2,18 +2,17 @@ package com.dandrzas.inertialsensorsviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
@@ -22,38 +21,68 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-   // DataPoint[] dataSeries = new DataPoint[200];
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     GraphView graph;
-    long i =0;
-    long startTime = -1;
-    LineGraphSeries<DataPoint> lineGraphSeries= new LineGraphSeries<>();
+    LineGraphSeries<DataPoint> graphSeriesX = new LineGraphSeries<>();
+    LineGraphSeries<DataPoint> graphSeriesY = new LineGraphSeries<>();
+    LineGraphSeries<DataPoint> graphSeriesZ = new LineGraphSeries<>();
+
+    private int graphMaxY = 40;
+    private final int GRAPH_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        graph = (GraphView) findViewById(R.id.graph);
-        Button button = findViewById(R.id.button);
-        lineGraphSeries.setThickness(2);
-        graph.addSeries(lineGraphSeries);
-        Viewport viewport = graph.getViewport();
-        viewport.setMinY(-40);
-        viewport.setMaxY(40);
-        viewport.setYAxisBoundsManual(true);
+        graph =  findViewById(R.id.graph_accelerometer);
+        Button button_zoom_in = findViewById(R.id.button_zoom_in);
+        Button button_zoom_out = findViewById(R.id.button_zoom_out);
 
+        // graph series config
+        graphSeriesX.setThickness(2);
+        graphSeriesX.setColor(Color.BLUE);
+        graph.addSeries(graphSeriesX);
+        graphSeriesY.setThickness(2);
+        graph.addSeries(graphSeriesY);
+        graphSeriesY.setColor(Color.RED);
+        graphSeriesZ.setThickness(2);
+        graphSeriesZ.setColor(Color.GREEN);
+        graph.addSeries(graphSeriesZ);
+
+        //graph config
+        Viewport viewport = graph.getViewport();
+        viewport.setMinY((-1)*graphMaxY);
+        viewport.setMaxY(graphMaxY);
+        viewport.setYAxisBoundsManual(true);
         viewport.setMinX(0);
-        viewport.setMaxX(1000);
+        viewport.setMaxX(GRAPH_LENGTH);
         viewport.setXAxisBoundsManual(true);
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
         staticLabelsFormatter.setHorizontalLabels(new String[] {"", "", "","","","","","",""});
         graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
         graph.getGridLabelRenderer().setNumVerticalLabels(9);
+        // legend
+        graphSeriesX.setTitle("Oś X");
+        graphSeriesY.setTitle("Oś Y");
+        graphSeriesZ.setTitle("Oś Z");
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setBackgroundColor(Color.LTGRAY);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
 
-        button.setOnClickListener(view-> {
-        ;
-                });
+        button_zoom_in.setOnClickListener(view-> {
+            graphMaxY = graphMaxY/2;
+            if(graphMaxY<1) graphMaxY=1;
+            viewport.setMinY((-1)*graphMaxY);
+            viewport.setMaxY(graphMaxY);
+        });
+
+        button_zoom_out.setOnClickListener(view-> {
+            graphMaxY = graphMaxY*2;
+            if(graphMaxY>100) graphMaxY=100;
+            viewport.setMinY((-1)*graphMaxY);
+            viewport.setMaxY(graphMaxY);
+        });
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -66,22 +95,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onPause() {
         super.onPause();
-        //mSensorManager.unregisterListener(this);
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        if (startTime == -1)  startTime=event.timestamp;
-        double timeStamp = (double)(event.timestamp - startTime)/1000000;
-        Log.d("sensorstest time", Double.toString(timeStamp));
-        Log.d("sensorstest value", (event.values[0]) + "\n");
-
         boolean scrollToEnd = false;
-        if  (i>=1000)scrollToEnd = true;
-        lineGraphSeries.appendData(new DataPoint(i, event.values[0]), scrollToEnd, 1000);
-        i++;
+        if  (graphSeriesY.getHighestValueX()>=GRAPH_LENGTH)scrollToEnd = true;
+        graphSeriesX.appendData(new DataPoint(graphSeriesX.getHighestValueX()+1, event.values[1]), scrollToEnd, GRAPH_LENGTH);
+        graphSeriesY.appendData(new DataPoint(graphSeriesY.getHighestValueX()+1, event.values[0]), scrollToEnd, GRAPH_LENGTH);
+        graphSeriesZ.appendData(new DataPoint(graphSeriesZ.getHighestValueX()+1, event.values[2]), scrollToEnd, GRAPH_LENGTH);
+
     }
 
     @Override
